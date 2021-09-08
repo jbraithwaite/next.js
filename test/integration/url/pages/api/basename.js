f292d090dafcc76f37019e3717abe8a213064d82
Tobias Koppers <tobias.koppers@googlemail.com>
2021-09-09 00:00:43 +0200

    add support for new URL()
    Introduce separate layer for api entries, which uses server-side URLs.
    Otherwise new URL() will return a faked relative URL, that is identical in SSR and CSR

---
+++ test/integration/url/pages/api/basename.js
@@ -0,0 +1,8 @@
+import fs from 'fs'
+import path from 'path'
+
+const img = new URL('../../public/vercel.png', import.meta.url)
+
+export default (req, res) => {
+  res.json({ basename: path.posix.basename(img.pathname) })
+}
